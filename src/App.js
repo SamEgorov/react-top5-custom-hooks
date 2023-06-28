9ee8d6861df0a1f96f9823909501d61e966886c2
Semyon Egorov <sam.egorov@gmail.com>
2023-06-28 04:10:45 +0300

    Update example for useInput hook

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {useState} from 'react';
 import useInput from "./hooks/useInput";
 
 const App = () => {
-    const name = useInput('John')
+    const login = useInput('')
+    const pass = useInput('')
 
     return (
         <div>
-            <h1>{name.value}</h1>
-            <input type="text" onChange={(e) => name.onChange(e)} value={name.value}/>
+            <h1>{login.value}</h1>
+            <input type="text" placeholder='Login' onChange={(e) => login.onChange(e)} value={login.value}/>
+            <input type="password" placeholder='Password' onChange={(e) => pass.onChange(e)} value={pass.value}/>
         </div>
     );
 };
