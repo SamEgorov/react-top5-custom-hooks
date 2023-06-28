af1a8aa9c6aa375d457481acbacfca2163784c36
Semyon Egorov <sam.egorov@gmail.com>
2023-06-28 04:07:24 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import useInput from "./hooks/useInput";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const name = useInput('John')
+
+    return (
+        <div>
+            <h1>{name.value}</h1>
+            <input type="text" onChange={(e) => name.onChange(e)} value={name.value}/>
+        </div>
+    );
+};
 
 export default App;
